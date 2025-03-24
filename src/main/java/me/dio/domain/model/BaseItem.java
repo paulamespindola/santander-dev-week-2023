a36a8265f0f179047fab4cf7acd5417210b03147
paulamespindola <paulamespindola04@gmail.com>
2025-03-24 15:34:09 -0300

    implementação da camada de domínio

---
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -0,0 +1,22 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Setter
+@Getter
+public abstract class BaseItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String icon;
+
+    private String description;
+}
